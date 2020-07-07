518958b3f2f207945b75ed8248ae19081b7ad2a1
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2020-07-07 15:56:41 +0200

    2nd module done

---
+++ src/QuizQuestion.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+
+class QuizQuestion extends Component{
+
+    constructor(props){
+        super(props);
+        this.quiz_question = props.quiz_question
+    }
+
+
+    render(){
+
+        return(
+            <main>
+                <section>
+                    <p>{this.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>{this.quiz_question.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        );
+
+    }
+
+}
+
+export default QuizQuestion;
