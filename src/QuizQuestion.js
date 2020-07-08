552472d652493825b4b285b5104c5da1fbe8484a
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2020-07-08 12:25:44 +0200

    4th module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react'
-import QuizQuestionButton from './QuizQuestionButton';
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
+class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.quiz_question = props.quiz_question;
+  }
 
-
-class QuizQuestion extends Component{
-
-    constructor(props){
-        super(props);
-        this.quiz_question = props.quiz_question
-    }
-
-
-    render(){
-
-        return(
-            <main>
-                <section>
-                    <p>{this.quiz_question.instruction_text}</p>
-                </section>
-                <section className="buttons">
-                    <ul>
-                        <QuizQuestionButton button_text={this.quiz_question.answer_options[0]} />
-                    </ul>
-                </section>
-            </main>
-        );
-
-    }
-
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.quiz_question.answer_options.map((answer) => (
+              <QuizQuestionButton
+                button_text={answer}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default QuizQuestion;
