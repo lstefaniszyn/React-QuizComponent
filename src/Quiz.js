71f8f986f5749d9f801a5b9eaae015fced7a3694
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2020-07-08 12:28:30 +0200

    resolved 4th module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,32 @@
-import React, {useState, Component} from 'react';
-import QuizQuestion from './QuizQuestion';
-import QuizEnd from './QuizEnd';
+import React, { useState, Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
-let quizData = require('./quiz_data.json');
+let quizData = require("./quiz_data.json");
 
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
 
-class Quiz extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {quiz_position: 1};
-
-    }
-
-    render(){
-        const isQuizEnd = (this.state.quiz_position-1) === quizData.quiz_questions.length;
-        return (<div>
-            {isQuizEnd && <QuizEnd />}
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
-        </div>);
-    }
-
+  render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
+      </div>
+    );
+  }
 }
 
-export default Quiz
+export default Quiz;
